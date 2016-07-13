81e269e25c411e2dc3b951dc0d697f3b13917bbb
Sam Sartor <mrocclusion@gmail.com>
2016-07-13 18:06:00 +0200

    Random Fade

--- script.js
+++ script.js
@@ -60,6 +60,10 @@ function resizeCanvas() {
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
 	t.update(canvas);
+	ctx.restore();
+	ctx.save();
+	t.applyToCanvas(ctx);
+	clear();
 	draw();
 }
 window.addEventListener('resize', resizeCanvas, false);
@@ -68,19 +72,32 @@ resizeCanvas();
 // Start code
 
 function clear() {
-	ctx.restore();
-	ctx.save();
-	t.applyToCanvas(ctx);
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
+
+function randColor(x, y, t) {
+	var m = 5664;
+	var v = ((x * 545425) ^ (y * 85675)) % m;
+	v /= m;
+	return .5 + .5 * Math.sin(v * Math.PI * 2 + t);
+}
+
+function scaleColor(s, r0, g0, b0, r1, g1, b1) {
+	var r = Math.floor(s * (r1 - r0) + r0);
+	var g = Math.floor(s * (g1 - g0) + g0);
+	var b = Math.floor(s * (b1 - b0) + b0);
+	return (r << 16) | (g << 8) | b;
+}
+
+var age = 0;
 
 function draw() {
 	clear();
 	for (var x = 0; x < 100; x += 10) {
 		for (var y = 0; y < 100; y += 10) {
-			
-			if ((Math.floor(mouseX / 10) * 10) == x && y == (Math.floor(mouseY / 10) * 10)) ctx.fillStyle = "#08476D";
-			else ctx.fillStyle = "#2B2B2C";
+			//if ((Math.floor(mouseX / 10) * 10) == x && y == (Math.floor(mouseY / 10) * 10)) ctx.fillStyle = "#08476D";
+			//else ctx.fillStyle = "#2B2B2C";
+			ctx.fillStyle = "#" + scaleColor(randColor(x, y, age * .2), 0x2B, 0x2B, 0x2C, 0x08, 0x47, 0x6D).toString(16);
 			ctx.fillRect(x, y, 9.8, 9.8);
 		}
 	}
@@ -92,5 +109,12 @@ function mouseDetector(event) {
 	var p = t.fromScreen(sx, sy);
 	mouseX = p.x;
 	mouseY = p.y; 
+	//draw();
+}
+
+function tick() {
+	age++;
 	draw();
 }
+
+var tickID = window.setInterval(tick, 100);
